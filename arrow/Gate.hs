b03e3c50fa49ce1c637e3c1d8b206b2c68bc2b39
Miëtek Bak <mietek@varsztat.com>
2010-06-18 19:49:43 +0100

    Fix finrod's gate function

--- arrow/Gate.hs
+++ arrow/Gate.hs
@@ -9,6 +9,7 @@ import Filter
 
 
 data Trit = T0 | T1 | T2
+  deriving Eq
 
 instance Show Trit where
   show T0 = "0"
@@ -55,11 +56,47 @@ finrod (l, r) =
       (T0, T1) -> (T2, T2)
       (T0, T2) -> (T1, T2)
       (T1, T0) -> (T1, T2)
-      (T1, T1) -> (T2, T0)
+      (T1, T1) -> (T0, T0)
       (T1, T2) -> (T2, T1)
       (T2, T0) -> (T2, T2)
-      (T2, T1) -> (T1, T0)
+      (T2, T1) -> (T1, T1)
       (T2, T2) -> (T0, T0)
 
 input :: [Trit]
 input = [T0, T1, T2, T0, T2, T1, T0, T1, T2, T1, T0, T2, T0, T1, T2, T0, T2]
+
+output1 :: [Trit]
+output1 = [T0, T2, T1, T2, T0, T1, T1, T2, T1, T0, T0, T0, T0, T2, T1, T2, T0]
+
+output2 :: [Trit]
+output2 = [T2, T2, T1, T2, T0, T2, T2, T1, T0, T2, T2, T0, T2, T2, T1, T2, T0]
+
+output3 :: [Trit]
+output3 = [T0, T1, T2, T1, T0, T2, T2, T1, T2, T0, T0, T0, T0, T1, T2, T1, T0]
+
+output4 :: [Trit]
+output4 = [T2, T2, T0, T2, T2, T0, T2, T2, T0, T2, T2, T0, T2, T2, T0, T2, T2]
+
+test1 :: [Trit]
+test1 =
+  let g = mkGate finrod
+      c1 = mkConfig1 g
+  in run c1 input
+
+test2 :: [Trit]
+test2 =
+  let g = mkGate finrod
+      c2 = mkConfig2 g
+  in run c2 input
+
+test3 :: [Trit]
+test3 =
+  let g = mkGate finrod
+      c3 = mkConfig3 g
+  in run c3 input
+
+test4 :: [Trit]
+test4 =
+  let g = mkGate finrod
+      c4 = mkConfig4 g
+  in run c4 input
