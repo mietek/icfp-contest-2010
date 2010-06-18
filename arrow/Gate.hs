ced6be26d1d5163fd4995fdafe6d8fd6e82a36f4
Miëtek Bak <mietek@varsztat.com>
2010-06-18 19:27:38 +0100

    Add finrod's flawed function

--- arrow/Gate.hs
+++ arrow/Gate.hs
@@ -23,24 +23,39 @@ mkGate f = lift f
 
 mkConfig1 :: Gate -> Circuit
 mkConfig1 g = proc x -> do
-  rec (y, r) <- g -< (x, r')
-      r' <- delay T0 -< r
+  rec r <- delay T0 -< r'
+      (y, r') <- g -< (x, r)
   returnA -< y
 
 mkConfig2 :: Gate -> Circuit
 mkConfig2 g = proc x -> do
-  rec (r, y) <- g -< (x, r')
-      r' <- delay T0 -< r
+  rec r <- delay T0 -< r'
+      (r', y) <- g -< (x, r)
   returnA -< y
 
 mkConfig3 :: Gate -> Circuit
 mkConfig3 g = proc x -> do
-  rec (y, r) <- g -< (r', x)
-      r' <- delay T0 -< r
+  rec r <- delay T0 -< r'
+      (y, r') <- g -< (r, x)
   returnA -< y
 
 mkConfig4 :: Gate -> Circuit
 mkConfig4 g = proc x -> do
-  rec (r, y) <- g -< (r', x)
-      r' <- delay T0 -< r
+  rec r <- delay T0 -< r'
+      (r', y) <- g -< (r, x)
   returnA -< y
+
+
+finrod :: (Trit, Trit) -> (Trit, Trit)
+finrod (T0, T0) = (T0, T2)
+finrod (T0, T1) = (T2, T2)
+finrod (T0, T2) = (T1, T2)
+finrod (T1, T0) = (T1, T2)
+finrod (T1, T1) = (T2, T0)
+finrod (T1, T2) = (T2, T1)
+finrod (T2, T0) = (T2, T2)
+finrod (T2, T1) = (T1, T0)
+finrod (T2, T2) = (T0, T0)
+
+input :: [Trit]
+input = [T0, T1, T2, T0, T2, T1, T0, T1, T2, T1, T0, T2, T0, T1, T2, T0, T2]
