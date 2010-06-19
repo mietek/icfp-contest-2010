c516f7ba49d5f54aad1f8c4da80a03c18299d377
Maciej Kotowicz <kotowicz.maciej@gmail.com>
2010-06-19 16:23:00 +0200

    ...

--- arrow/Circuit.hs
+++ arrow/Circuit.hs
@@ -262,10 +262,10 @@ genP1 c inp out k = genFooToN c inp (show (c + (17-k) *3) ++  "R") (17-k) ++
                     genFooToN (c + (17-k)*3 +1) (show (c + (17-k)*3) ++ "R") out k
 
 
-genDodaj c inp out 0 = p1 ++ inp ++ (show (c+lp1) ++ "R") ++ "0#" ++ out ++  (show (c + 1)++"L") ++ ",\n"
+genDodaj c inp out 0 = p1 ++ inp ++ (show (c+lp1-2) ++ "R") ++ "0#" ++ out ++  (show (c + 1)++"L") ++ ",\n"
     where p1 = genP1 c (show (c+lp1) ++ "R") (show (c+lp1) ++"R") 0
           lp1 = length $ lines $ genP1 0 "" "" 0
-genDodaj c inp out k = p1 ++ inp ++ (show (c+lp1 -1) ++ "L") ++ "0#" ++ out ++ (show (c +1)++"L") ++ ",\n"
+genDodaj c inp out k = p1 ++ inp ++ (show (c+lp1 -3) ++ "L") ++ "0#" ++ out ++ (show (c +1)++"L") ++ ",\n"
     where p1 = genP1 c (show (c+lp1) ++ "R") (show (c+lp1) ++"R") k
           lp1 = length $ lines $ genP1 0 "" "" k
 
