20b9c818142a87e862ae68bfe3e80fd44a5e1a23
Miëtek Bak <mietek@varsztat.com>
2010-06-20 14:50:46 +0100

    Support more errors in solve_car

--- scrape/solve_car.py
+++ scrape/solve_car.py
@@ -27,7 +27,7 @@ def solve_car(car, circuit):
       print pre.string.strip()
     errors = title_div.find("span", attrs={"id": "solution.errors"})
     if errors and errors.string:
-      if errors.string == "You have already submitted this solution.":
+      if errors.string.startswith("You have already submitted this solution"):
         solved = True
       ok = True
       print errors.string.strip()
@@ -36,9 +36,11 @@ def solve_car(car, circuit):
     if main_div:
       msg_div = main_div.find("div", attrs={"version": "2.0"})
       if msg_div and msg_div.contents and len(msg_div.contents) > 1:
-        solved = True
-        ok = True
-        print msg_div.contents[0].strip()
+        msg = str(msg_div.contents[0]).strip()
+        if msg.startswith("You have submitted fuel for car"):
+          solved = True
+          ok = True
+          print msg
       pre = main_div.find("pre")
       if pre and pre.string:
         ok = True
@@ -54,4 +56,4 @@ if __name__ == '__main__':
     sys.exit()
   login.login()
   if solve_car(sys.argv[1], sys.argv[2]):
-    print "(*** SOLVED ***)" + "\007"
+    print "*** SOLVED ***" + "\007"
