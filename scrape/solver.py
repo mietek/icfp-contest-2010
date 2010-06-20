6a402c0c562f27a921ac764bd1751dd8e88b2a98
Maciej Kotowicz <kotowicz.maciej@gmail.com>
2010-06-21 01:34:48 +0200

    solver.py: new solver script

---
+++ scrape/solver.py
@@ -0,0 +1,28 @@
+
+import solve_car
+import sys
+from subprocess import Popen, PIPE
+
+'''
+<jmi> wczyta liste par nr, liczba
+<jmi> wygeneruje ./circuits liczba
+<jmi> i uruchomi solve_car nr wynikGeneracji
+'''
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print "Usage: solver.py <file_with_pairs>"
+    sys.exit()
+
+  for line in file(sys.argv[1]).readlines():
+      if line[:1] != 'n':
+
+          tmp = line.split(',')
+          circ = Popen(["../circuit/factcomp",tmp[1]], stdout=PIPE).communicate()[0]
+          file('tmp.circ','w').write(circ)
+          if solve_car.solve_car(tmp[0], 'tmp.circ'):
+              print "*** SOLVED ***" + "\007"
+
+
+
+
