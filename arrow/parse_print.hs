b3da265b0d421a868de4874efb5e529547fc0b0e
clfp-jami <przecinq+gh@gmail.com>
2010-06-21 01:20:10 +0200

    poprawki i mniejsze przedzialy

--- arrow/parse_print.hs
+++ arrow/parse_print.hs
@@ -1,4 +1,5 @@
 import Data.List
+import System.IO
 
 pow3s = map (3^) [0..]
 
@@ -112,18 +113,18 @@ doListy5 (v1, v2, v3, v4, v5) = printFuel [[[v1]],[[v2]],[[v3]],[[v4]],[[v5]]]
 parse6 nr w e = if ([] == rozw)
        then putStr ("nierozwiazane:" ++ (show nr) ++ "," ++ w)
        else putStr ((show nr)++ "," ++ (doListy (head rozw)))
-  where rozw = [ (v1,v2,v3,v4,v5,v6) | v6 <- [1..30], v5 <- [1..30], v3 <- [1..30], v4 <- [1..30], v2 <- [1..30], v1 <- [1..30], (sprawdzRownania e (v1,v2,v3,v4,v5,v6))]
+  where rozw = [ (v1,v2,v3,v4,v5,v6) | v6 <- [1..25], v5 <- [1..25], v3 <- [1..25], v4 <- [1..25], v2 <- [1..25], v1 <- [1..25], (sprawdzRownania e (v1,v2,v3,v4,v5,v6))]
 
 parse5 nr w e = if ([] == rozw)
        then putStr ("nierozwiazane:" ++ (show nr) ++ "," ++ w ++ "\n")
        else putStr ((show nr)++ "," ++ (doListy5 (head rozw)) ++ "\n")
-  where rozw = [ (v1,v2,v3,v4,v5) | v5 <- [1..32], v4 <- [1..32], v3 <- [1..64], v2 <- [1..64], v1 <- [1..64], sprawdzRownania5 e (v1,v2,v3,v4,v5)]
+  where rozw = [ (v1,v2,v3,v4,v5) | v5 <- [1..32], v4 <- [1..32], v3 <- [1..32], v2 <- [1..32], v1 <- [1..64], sprawdzRownania5 e (v1,v2,v3,v4,v5)]
 
 parseE1 :: (Int, String) -> IO ()
 parseE1 (nr,w) = if (czySzesc e) then parse6 nr w e  else parse5 nr w e
+   >>
+  (hFlush stdout)
   where (e, s) = parseEngine w
-
-
 
 
 main = (map read . lines) `fmap` readFile "cars_list" >>= mapM_ parseE1
