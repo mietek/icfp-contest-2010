7952dfd914ca78ff9662f2e0962aabaeb33f713d
Maciej Kotowicz <kotowicz.maciej@gmail.com>
2010-06-21 01:44:24 +0200

    solver: added autorization

--- scrape/solver.py
+++ scrape/solver.py
@@ -1,6 +1,7 @@
 
 import solve_car
 import sys
+import login
 from subprocess import Popen, PIPE
 
 '''
@@ -14,14 +15,17 @@ if __name__ == '__main__':
     print "Usage: solver.py <file_with_pairs>"
     sys.exit()
 
+  login.login()
   for line in file(sys.argv[1]).readlines():
       if line[:1] != 'n':
 
           tmp = line.split(',')
           circ = Popen(["../circuit/factcomp",tmp[1]], stdout=PIPE).communicate()[0]
           file('tmp.circ','w').write(circ)
+#          login.login()
           if solve_car.solve_car(tmp[0], 'tmp.circ'):
               print "*** SOLVED ***" + "\007"
+              print "\n"
 
 
 
